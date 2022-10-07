1f5c51aeb60bf09ce5538ae9104d8b39701d690a
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-07 15:10:44 +0530

    added mastercard validation code

---
+++ mastercard.py
@@ -0,0 +1,17 @@
+import re
+
+def checkMasterCardNo(cardNo):
+    regex = "^5[1-5][0-9]{14}|^(222[1-9]|22[3-9]\\d|2[3-6]\\d{2}|27[0-1]\\d|2720)[0-9]{12}$"
+    r = re.compile(regex)
+
+    if(re.search(r, cardNo)):
+        print("Valid")
+    else:
+        print("Not Valid")
+ 
+
+card1 = "5110762060017101"
+checkMasterCardNo(card1)
+
+card1 = "8632458236982734"
+checkMasterCardNo(card1)
